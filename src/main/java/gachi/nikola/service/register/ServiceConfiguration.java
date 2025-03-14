72daf74bfe7d9b94b94c2742cf9bcc8bb0dc8b28
gachiCULTIST <301nikotrin301@gmail.com>
2025-03-14 22:52:38 +0300

    init commit

---
+++ src/main/java/gachi/nikola/service/register/ServiceConfiguration.java
@@ -0,0 +1,9 @@
+package gachi.nikola.service.register;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"controller", "service", "repository"})
+public class ServiceConfiguration {
+}
---
+++ src/main/java/gachi/nikola/service/register/dto/RegisterRequest.java
@@ -0,0 +1,7 @@
+package gachi.nikola.service.register.dto;
+
+public class RegisterRequest {
+
+    private final String login;
+    private final String email;
+}
